5c2eca697018285ae4270482e719aa721d974ede
Demian <hagen_id@ukr.net>
2020-01-23 10:15:45 +0200

    create main.go file
    Work with create file

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// initialise verable
+	text := "Hello Gold!"
+	filename := "hello.txt"
+	// Chek existing file, if existing function is close
+	if _, err := os.Stat(filename); !os.IsNotExist(err) {
+		fmt.Println("Такой файл уже существует:", filename)
+		os.Exit(0)
+	} else {
+		fmt.Println("test1")
+	}
+
+	file, err := os.Create(filename)
+
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		return
+	}
+	defer file.Close()
+	file.WriteString(text)
+
+	fmt.Println("Done.")
+}
