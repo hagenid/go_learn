5391511302efcb2eefc703efd4708294edf7b486
Demian <hagen_id@ukr.net>
2020-01-30 12:16:16 +0200

    add Dockerfile

--- main.go
+++ main.go
@@ -6,11 +6,26 @@ import (
 	"main/utils"
 )
 
+type Person struct {
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	isAdmin bool   `json:"isadmin"`
+}
+
 func main() {
 	color.Red.Println("test")
 	color.Red.Println(cmd.A)
 	color.Green.Println(utils.R)
 	color.Green.Println(utils.C)
+
+	p := Person{
+		Name:    "Mike",
+		Age:     16,
+		isAdmin: false,
+	}
+
+	color.Red.Println(p, p.Name, p.Age, p.isAdmin)
+
 }
 
 // create new MyObject
